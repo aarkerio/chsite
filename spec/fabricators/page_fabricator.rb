7c12166aa76fba7097508aead1d59b0de26c60c1
aarkerio <mmontoya@gmail.com>
2014-01-13 13:03:27 -0600

    New Rspec classes

--- spec/fabricators/page_fabricator.rb
+++ spec/fabricators/page_fabricator.rb
@@ -1,5 +1,17 @@
+# Chipotle Software (c) 2014 GPLv3
+
 Fabricator(:page) do
   title 'Foobar'
-  email { Faker::Internet.email }
-  
+  body    
+  discution 
+  published 
+  visits     
+  tags      
+  rank      
+  cv      
+  editor     
+  user_id    
+  created    
+  section_id 
+  slug        
 end
